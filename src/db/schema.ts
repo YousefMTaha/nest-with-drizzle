1933b91c18b2d5b3cb54175765b87f4831e7f20c
yousef <tyousef262@gmail.com>
2025-06-19 15:55:50 +0300

    fix(Schema): fix bug

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,27 +9,7 @@ import {
   } from 'drizzle-orm/pg-core';
   import { relations } from 'drizzle-orm';
   
-  export const skus = pgTable('sku', {
-    id: serial().primaryKey(),
-    name: text().notNull(),
-    skuCode: text('sku_code').unique().notNull(),
-    category: text().notNull(),
-    subcategory: text('').notNull(),
-    brandName: text('brand_name').notNull(),
-    isActive: boolean('is_active').default(true),
-    branchId: serial('branch_id').references(() => inventoryBranches.id),
-    barcodeData: text('barcode_data'),
-    qrCodeData: text('qr_code_data'),
-    createdAt: timestamp('created_at').defaultNow().notNull(),
-    updatedAt: timestamp('updated_at').defaultNow().notNull(),
-  });
-  
-  export const skuRelations = relations(skus, ({ one }) => ({
-    branch: one(inventoryBranches, {
-      fields: [skus.branchId],
-      references: [inventoryBranches.id],
-    }),
-  }));
+
 export const inventoryBranches = pgTable('inventory_branch', {
   id: serial().primaryKey(),
   name: text('name').notNull(),
@@ -51,3 +31,25 @@ export const inventoryBranchRelations = relations(
     skus: many(skus),
   }),
 );
+
+  export const skus = pgTable('sku', {
+    id: serial().primaryKey(),
+    name: text().notNull(),
+    skuCode: text('sku_code').unique().notNull(),
+    category: text().notNull(),
+    subcategory: text('').notNull(),
+    brandName: text('brand_name').notNull(),
+    isActive: boolean('is_active').default(true),
+    branchId: serial('branch_id').references(() => inventoryBranches.id),
+    barcodeData: text('barcode_data'),
+    qrCodeData: text('qr_code_data'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  });
+  
+  export const skuRelations = relations(skus, ({ one }) => ({
+    branch: one(inventoryBranches, {
+      fields: [skus.branchId],
+      references: [inventoryBranches.id],
+    }),
+  }));
